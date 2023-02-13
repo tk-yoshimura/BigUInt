7b2bca0d46ad2624ecea8f9c980d6fcafa6adbe2
tk-yoshimura <yoshimura8192@gmail.com>
2023-02-14 01:47:28 +0900

    Initial commit

---
+++ BigUInt/UIntUtil_pack.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BigUInt {
+    internal static partial class UIntUtil {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (UInt32 high, UInt32 low) Unpack(UInt64 v) {
+            UInt32 low = unchecked((UInt32)v);
+            UInt32 high = unchecked((UInt32)(v >> UInt32Bits));
+
+            return (high, low);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UInt64 Pack(UInt32 high, UInt32 low) {
+            return (((UInt64)high) << UInt32Bits) | ((UInt64)low);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (UInt32 high, UInt32 low) DecimalUnpack(UInt64 v) {
+
+            UInt32 high, low;
+
+#if DEBUG
+            checked
+#else
+            unchecked
+#endif
+            {
+                high = (UInt32)(v / UInt32MaxDecimal);
+                low = (UInt32)(v - (UInt64)high * (UInt64)UInt32MaxDecimal);
+            }
+
+            return (high, low);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (UInt32 high, UInt32 low) DecimalUnpack(UInt32 v) {
+            UInt32 high = v / UInt32MaxDecimal;
+            UInt32 low = v - high * UInt32MaxDecimal;
+
+            return (high, low);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UInt64 DecimalPack(UInt32 high, UInt32 low) {
+            return (UInt64)high * (UInt64)UInt32MaxDecimal + (UInt64)low;
+        }
+    }
+}
---
+++ BigUIntTest/BigUIntTest.cs
@@ -0,0 +1,45 @@
+using BigUInt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Numerics;
+
+namespace BigUIntTest {
+    [TestClass]
+    public class UInt128Test {
+        [TestMethod]
+        public void ToStringTest() {
+            UInt128 v0 = new(0uL, 0uL);
+            UInt128 v1 = new(0uL, 1uL);
+            UInt128 v2 = new(0uL, 10uL);
+            UInt128 v3 = new(0uL, 100uL);
+            UInt128 v4 = new(0uL, 10000000000000uL);
+            UInt128 v5 = new(0x00000000033b2e3cuL, 0x9fd0803ce8000000uL);
+            UInt128 v6 = new(0x0001ed09bead87c0uL, 0x378d8e6400000000uL);
+            UInt128 v7 = new(UInt64.MaxValue, UInt64.MaxValue);
+
+            Assert.AreEqual("0", v0.ToString());
+            Assert.AreEqual("1", v1.ToString());
+            Assert.AreEqual("10", v2.ToString());
+            Assert.AreEqual("100", v3.ToString());
+            Assert.AreEqual("10000000000000", v4.ToString());
+            Assert.AreEqual("1000000000000000000000000000", v5.ToString());
+            Assert.AreEqual("10000000000000000000000000000000000", v6.ToString());
+            Assert.AreEqual("340282366920938463463374607431768211455", v7.ToString());
+        }
+
+        [TestMethod]
+        public void BigIntegarTest() {
+            BigInteger v = (((BigInteger)UInt64.MaxValue) << 64) + UInt64.MaxValue;
+            BigInteger mask = UInt64.MaxValue;
+
+            while (v > 0) {
+                UInt128 u = new((UInt64)((v >> 64) & mask), (UInt64)(v & mask));
+
+                Console.WriteLine(v);
+
+                Assert.AreEqual(v.ToString(), u.ToString());
+
+                v /= 3;
+            }
+        }
+    }
+}
