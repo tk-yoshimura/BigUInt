0b728071be57ceb216257c46dabfc7c0cb25d03a
tk-yoshimura <yoshimura8192@gmail.com>
2023-02-27 22:54:14 +0900

    + fms

---
+++ AvxUInt/UIntUtil/UIntUtil_empty.cs
@@ -0,0 +1,9 @@
+﻿//using System;
+//using System.Runtime.Intrinsics;
+//using static System.Runtime.Intrinsics.X86.Avx;
+//using static System.Runtime.Intrinsics.X86.Avx2;
+
+namespace AvxUInt {
+    internal static partial class UIntUtil {
+    }
+}
