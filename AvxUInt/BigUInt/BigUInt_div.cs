b7fa20a6c22b5ab7ab2744b83adbe534cfddfbf2
tk-yoshimura <yoshimura8192@gmail.com>
2023-03-02 05:43:44 +0900

    fix div

--- AvxUInt/BigUInt/BigUInt_div.cs
+++ AvxUInt/BigUInt/BigUInt_div.cs
@@ -2,43 +2,43 @@
     public sealed partial class BigUInt<N> {
 
         public static BigUInt<N> operator /(BigUInt<N> a, BigUInt<N> b) {
-            return DivRem(a, b).q;
+            return Div(a, b);
         }
 
         public static BigUInt<N> operator /(BigUInt<N> a, UInt32 b) {
-            return DivRem(a, b).q;
+            return Div(a, b);
         }
 
         public static BigUInt<N> operator /(UInt32 a, BigUInt<N> b) {
-            return DivRem(a, b).q;
+            return Div(a, b);
         }
 
         public static BigUInt<N> operator /(BigUInt<N> a, UInt64 b) {
-            return DivRem(a, b).q;
+            return Div(a, b);
         }
 
         public static BigUInt<N> operator /(UInt64 a, BigUInt<N> b) {
-            return DivRem(a, b).q;
+            return Div(a, b);
         }
 
         public static BigUInt<N> operator %(BigUInt<N> a, BigUInt<N> b) {
-            return DivRem(a, b).r;
+            return Rem(a, b);
         }
 
         public static UInt32 operator %(BigUInt<N> a, UInt32 b) {
-            return DivRem(a, b).r;
+            return Rem(a, b);
         }
 
         public static BigUInt<N> operator %(UInt32 a, BigUInt<N> b) {
-            return DivRem(a, b).r;
+            return Rem(a, b);
         }
 
         public static UInt64 operator %(BigUInt<N> a, UInt64 b) {
-            return DivRem(a, b).r;
+            return Rem(a, b);
         }
 
         public static BigUInt<N> operator %(UInt64 a, BigUInt<N> b) {
-            return DivRem(a, b).r;
+            return Rem(a, b);
         }
 
         public static (BigUInt<N> q, BigUInt<N> r) DivRem(BigUInt<N> a, BigUInt<N> b) {
@@ -49,12 +49,28 @@
             return (q, r);
         }
 
+        public static BigUInt<N> Div(BigUInt<N> a, BigUInt<N> b) {
+            return DivRem(a, b).q;
+        }
+
+        public static BigUInt<N> Rem(BigUInt<N> a, BigUInt<N> b) {
+            return DivRem(a, b).r;
+        }
+
         public static (BigUInt<N> q, UInt32 r) DivRem(BigUInt<N> a, UInt32 b) {
             BigUInt<N> q = Zero.Copy(), r = a.Copy();
 
             UIntUtil.DivRem(q.value, r.value, b);
 
             return (q, r[0]);
+        }
+
+        public static BigUInt<N> Div(BigUInt<N> a, UInt32 b) {
+            return DivRem(a, b).q;
+        }
+
+        public static UInt32 Rem(BigUInt<N> a, UInt32 b) {
+            return DivRem(a, b).r;
         }
 
         public static (BigUInt<N> q, UInt64 r) DivRem(BigUInt<N> a, UInt64 b) {
@@ -65,9 +81,17 @@
             return (q, UIntUtil.Pack(r[1], r[0]));
         }
 
-        public static (BigUInt<N> q, BigUInt<N> r) DivRem(UInt32 a, BigUInt<N> b) {
+        public static BigUInt<N> Div(BigUInt<N> a, UInt64 b) {
+            return DivRem(a, b).q;
+        }
+
+        public static UInt64 Rem(BigUInt<N> a, UInt64 b) {
+            return DivRem(a, b).r;
+        }
+
+        public static (UInt32 q, UInt32 r) DivRem(UInt32 a, BigUInt<N> b) {
             if (b.Digits > 1u) {
-                return (Zero, a);
+                return (0u, a);
             }
 
             UInt32 denom = b[0];
@@ -75,14 +99,54 @@
             return (a / denom, a % denom);
         }
 
-        public static (BigUInt<N> q, BigUInt<N> r) DivRem(UInt64 a, BigUInt<N> b) {
+        public static UInt32 Div(UInt32 a, BigUInt<N> b) {
+            if (b.Digits > 1u) {
+                return 0u;
+            }
+
+            UInt32 denom = b[0];
+
+            return a / denom;
+        }
+
+        public static UInt32 Rem(UInt32 a, BigUInt<N> b) {
+            if (b.Digits > 1u) {
+                return a;
+            }
+
+            UInt32 denom = b[0];
+
+            return a % denom;
+        }
+
+        public static (UInt64 q, UInt64 r) DivRem(UInt64 a, BigUInt<N> b) {
             if (b.Digits > 2u) {
-                return (Zero, a);
+                return (0uL, a);
             }
 
             UInt64 denom = UIntUtil.Pack(b[1], b[0]);
 
             return (a / denom, a % denom);
+        }
+
+        public static UInt64 Div(UInt64 a, BigUInt<N> b) {
+            if (b.Digits > 2u) {
+                return 0uL;
+            }
+
+            UInt64 denom = UIntUtil.Pack(b[1], b[0]);
+
+            return a / denom;
+        }
+
+        public static UInt64 Rem(UInt64 a, BigUInt<N> b) {
+            if (b.Digits > 2u) {
+                return a;
+            }
+
+            UInt64 denom = UIntUtil.Pack(b[1], b[0]);
+
+            return a % denom;
         }
 
         public static (BigUInt<M> q, BigUInt<N> r) DivRem<M>(BigUInt<M> a, BigUInt<N> b) where M : struct, IConstant {
@@ -92,6 +156,14 @@
             UIntUtil.DivRem(q.value, r.value, b.value);
 
             return (q, new BigUInt<N>(r.value[..Length], enable_clone: false));
+        }
+
+        public static BigUInt<M> Div<M>(BigUInt<M> a, BigUInt<N> b) where M : struct, IConstant {
+            return DivRem(a, b).q;
+        }
+
+        public static BigUInt<N> Rem<M>(BigUInt<M> a, BigUInt<N> b) where M : struct, IConstant {
+            return DivRem(a, b).r;
         }
 
         public static BigUInt<N> RoundDiv(BigUInt<N> a, BigUInt<N> b) {
