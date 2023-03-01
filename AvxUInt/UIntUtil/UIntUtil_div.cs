d35a759f7539d158d1347a93c1f4cbd53fc338f5
tk-yoshimura <yoshimura8192@gmail.com>
2023-03-01 15:50:31 +0900

    + div biguint/uint32,64

--- AvxUInt/UIntUtil/UIntUtil_div.cs
+++ AvxUInt/UIntUtil/UIntUtil_div.cs
@@ -24,7 +24,7 @@ namespace AvxUInt {
                 DivRem(digits_b, arr_q, arr_a, arr_b[..(int)digits_b]);
             }
         }
-                
+
         /// <summary>Operate uint32 array q += a / b, a = a % b</summary>
         private static void DivRem(uint digits_b, UInt32[] arr_q, UInt32[] arr_a, UInt32[] arr_b) {
             if (arr_a.Length < 1 || arr_b.Length < 1 || arr_a.Length < arr_b.Length) {
@@ -49,6 +49,10 @@ namespace AvxUInt {
                     sft = 0;
                 }
 
+#if DEBUG
+                Debug<ArithmeticException>.Assert(n > 0uL);
+#endif
+
                 uint sft_block = (uint)sft / UInt32Bits;
                 int sft_rem = sft % UInt32Bits;
 
@@ -68,19 +72,107 @@ namespace AvxUInt {
 
         /// <summary>Operate uint32 array q += a / b, a = a % b</summary>
         public static void DivRem(UInt32[] arr_q, UInt32[] arr_a, UInt32 b) {
-            DivRem(1u, arr_q, arr_a, new UInt32[] { b });
+            if (arr_a.Length < 1) {
+                throw new ArgumentException("invalid length", nameof(arr_a));
+            }
+
+            uint lzc_b = LeadingZeroCount(b);
+            UInt64 numer, denom = b << (int)lzc_b;
+            uint offset_r, offset_b = lzc_b + (uint)(arr_a.Length - 1) * UInt32Bits;
+
+            while (true) {
+                (numer, offset_r) = TopUInt64(arr_a);
+
+                if (offset_r >= offset_b) {
+                    break;
+                }
+
+                UInt64 n = numer / denom;
+                int sft = (int)offset_b - (int)offset_r - UInt32Bits;
+
+                if (sft < 0) {
+                    n >>= -sft;
+                    sft = 0;
+                }
+
+#if DEBUG
+                Debug<ArithmeticException>.Assert(n > 0uL);
+#endif
+
+                uint sft_block = (uint)sft / UInt32Bits;
+                int sft_rem = sft % UInt32Bits;
+
+                n <<= sft_rem;
+
+                (UInt64 n_hi, UInt64 n_lo) = Unpack(n);
+
+                Add(sft_block, arr_q, n);
+                Sub(sft_block, arr_a, b * n_lo);
+                Sub(sft_block + 1, arr_a, b * n_hi);
+            }
+
+            if (arr_a[0] >= b) {
+                Add(arr_q, 1u);
+                Sub(arr_a, b);
+            }
         }
 
         /// <summary>Operate uint32 array q += a / b, a = a % b</summary>
         public static void DivRem(UInt32[] arr_q, UInt32[] arr_a, UInt64 b) {
             (UInt32 b_hi, UInt32 b_lo) = Unpack(b);
 
-            if (b_hi == 0u) { 
+            if (b_hi == 0u) {
                 DivRem(arr_q, arr_a, b_lo);
                 return;
             }
 
-            DivRem(2u, arr_q, arr_a, new UInt32[] { b_lo, b_hi });
+            if (arr_a.Length < 2) {
+                throw new ArgumentException("invalid length", nameof(arr_a));
+            }
+
+            uint lzc_b = LeadingZeroCount(b);
+            UInt64 b_sft = b << (int)lzc_b;
+
+            UInt64 numer, denom = (b_sft >> UInt32Bits) + ((b_sft & UInt32.MaxValue) > 0u ? 1uL : 0uL);
+            uint offset_r, offset_b = lzc_b + (uint)(arr_a.Length - 2) * UInt32Bits;
+
+            while (true) {
+                (numer, offset_r) = TopUInt64(arr_a);
+
+                if (offset_r >= offset_b) {
+                    break;
+                }
+
+                UInt64 n = numer / denom;
+                int sft = (int)offset_b - (int)offset_r - UInt32Bits;
+
+                if (sft < 0) {
+                    n >>= -sft;
+                    sft = 0;
+                }
+
+#if DEBUG
+                Debug<ArithmeticException>.Assert(n > 0uL);
+#endif
+
+                uint sft_block = (uint)sft / UInt32Bits;
+                int sft_rem = sft % UInt32Bits;
+
+                n <<= sft_rem;
+
+                (UInt64 n_hi, UInt64 n_lo) = Unpack(n);
+
+                Add(sft_block, arr_q, n);
+                Sub(sft_block, arr_a, b_lo * n_lo);
+                Sub(sft_block + 1, arr_a, b_lo * n_hi);
+                Sub(sft_block + 1, arr_a, b_hi * n_lo);
+                Sub(sft_block + 2, arr_a, b_hi * n_hi);
+            }
+
+            if (Pack(arr_a[1], arr_a[0]) >= b) {
+                Add(arr_q, 1u);
+                Sub(arr_a, b);
+            }
         }
 
         private static UInt64 RoundDenom(uint digits_b, UInt32[] arr_b) {
