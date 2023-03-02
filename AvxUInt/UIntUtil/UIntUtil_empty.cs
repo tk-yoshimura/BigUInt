a4a801d456ac42c07226aad737a1502aeab3c379
tk-yoshimura <yoshimura8192@gmail.com>
2023-03-02 17:55:07 +0900

    split repository

--- AvxUInt/UIntUtil/UIntUtil_empty.cs
+++
@@ -1,9 +0,0 @@
-﻿//using System;
-//using System.Runtime.Intrinsics;
-//using static System.Runtime.Intrinsics.X86.Avx;
-//using static System.Runtime.Intrinsics.X86.Avx2;
-
-namespace AvxUInt {
-    internal static partial class UIntUtil {
-    }
-}
